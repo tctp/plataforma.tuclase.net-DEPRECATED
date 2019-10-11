4a09fc9fffd98ca70e670ef5b548bbcf6fd37b77
solivares1401 <seba_olivares@live.cl>
2019-10-11 12:22:20 -0300

    reformulacion contenidos

--- componentes/tctpMenu.js
+++ componentes/tctpMenu.js
@@ -9,18 +9,23 @@ const SubMenu = Menu.SubMenu;
 
 // example header component
 export default class extends React.Component {
-    
+
     state = {
         openKeys: [],
-        current: [],
+        current: [],        
         loading: true
     }
 
-    onMenuClick = (e) => {        
+    onMenuClick = (e) => {             
         this.setState({
             current: [e.key],
         });
     }    
+
+    onTitleSubMenuClick=(e)=>{        
+        let { sistemaActual, lang } = this.props;          
+        Router.push(`/${lang}/${sistemaActual}/${e.key}/home`)
+    }
 
     componentDidMount() {        
         let itemActual = Router.router.pathname.substring(Router.router.pathname.lastIndexOf("/")).substr(1);
@@ -29,62 +34,44 @@ export default class extends React.Component {
     }
 
     onOpenChange = (openKeys) => {             
-        this.setState({
-            openKeys:openKeys
-        });
+        this.setState({ openKeys });
     }
 
     render() {
         let { openKeys, current } = this.state;
-        let { sistemaActual, lang } = this.props;      
-
+        let { sistemaActual, lang } = this.props;                      
         if (sistemaActual == "tctp-lms-bs") {
             return <IntlProvider locale={lang} messages={mensajes[lang]}>                
                 <Menu mode="inline" openKeys={openKeys} selectedKeys={current} onOpenChange={this.onOpenChange} onClick={this.onMenuClick}>
-                    {/* <SubMenu key="administrador" title={<FormattedMessage id="administrador" defaultMessage="Administrador" />} onTitleClick={() => Router.push(`/${lang}/${sistemaActual}/administrador/home`)} >
-                        <SubMenu key="gestion-usuarios" title={<FormattedMessage id="gestionUsuarios" defaultMessage="Gestion de usuarios" />} className="menuTercerNivel">
-                            <Menu.Item key={`about`}><Link href={`/${lang}/${sistemaActual}/administrador/gestion-usuarios/about`}><a><FormattedMessage id="about" defaultMessage="Quienes somos" /></a></Link></Menu.Item>
-                            <Menu.Item key={`contacto`}><Link href={`/${lang}/${sistemaActual}/administrador/gestion-usuarios/contacto`}><a><FormattedMessage id="contacto" defaultMessage="Contacto" /></a></Link></Menu.Item>
-                        </SubMenu>
-                    </SubMenu>
-                    <SubMenu key="gestor" title={<FormattedMessage id="gestor" defaultMessage="Gestor campus" />} onTitleClick={() => Router.push(`/${lang}/${sistemaActual}/gestor/home`)}>
-                        <Menu.Item key={`tutorial`}><Link href={`/${lang}/${sistemaActual}/gestor/tutorial`}><a>Tutorial</a></Link></Menu.Item>
-                        <Menu.Item key={`documentos`}><Link href={`/${lang}/${sistemaActual}/gestor/documentos`}><a>Documentos</a></Link></Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="tutor" title={<FormattedMessage id="tutor" defaultMessage="Tutor" />} onTitleClick={() => Router.push(`/${lang}/${sistemaActual}/tutor/home`)}>
-                        <Menu.Item key={`blog`}><Link href={`/${lang}/${sistemaActual}/tutor/blog`}><a>Blog</a></Link></Menu.Item>
-                        <Menu.Item key={`documentacion`}><Link href={`/${lang}/${sistemaActual}/tutor/documentacion`}><a>Documentacion</a></Link></Menu.Item>
-                        <Menu.Item key={`video`}><Link href={`/${lang}/${sistemaActual}/tutor/video`}><a>Video</a></Link></Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="supervisorTutor" title={<FormattedMessage id="supervisorTutor" defaultMessage="Supervisor Tutor" />} onTitleClick={() => Router.push(`/${lang}/${sistemaActual}/supervisorTutor/home`)}>
-                        <Menu.Item key={`evaluacion`}><Link href={`/${lang}/${sistemaActual}/supervisorTutor/evaluacion`}><a>Evaluacion</a></Link></Menu.Item>
-                        <Menu.Item key={`tutoriales`}><Link href={`/${lang}/${sistemaActual}/supervisorTutor/tutoriales`}><a>Blog</a></Link></Menu.Item>
-                    </SubMenu> */}
-                    <SubMenu key="editor" title={<FormattedMessage id="editor" defaultMessage="Editor de cursos" />} onTitleClick={() => Router.push(`/${lang}/${sistemaActual}/editor/home`)}>
-                        <Menu.Item key={`herramientas-de-comunicacion`}><Link href={`/${lang}/${sistemaActual}/editor/herramientas-de-comunicacion`}><a>Herramientas de comunicación</a></Link></Menu.Item>
+                    <SubMenu key="fase-edicion" title={<FormattedMessage id="fase-edicion" defaultMessage="Fase de edicion" />} onTitleClick={this.onTitleSubMenuClick}>                        
                         <SubMenu key="evaluacion" title={<FormattedMessage id="evaluacion" defaultMessage="Evaluación" />} className="menuTercerNivel">
-                            <Menu.Item key={`cuestionarios`}><Link href={`/${lang}/${sistemaActual}/editor/evaluacion/cuestionarios`}><a><FormattedMessage id="cuestionarios" defaultMessage="Cuestionarios" /></a></Link></Menu.Item>
-                            <Menu.Item key={`buzon`}><Link href={`/${lang}/${sistemaActual}/editor/evaluacion/buzon`}><a><FormattedMessage id="buzon" defaultMessage="Buzon" /></a></Link></Menu.Item>
-                            <Menu.Item key={`calificaciones`}><Link href={`/${lang}/${sistemaActual}/editor/evaluacion/calificaciones`}><a><FormattedMessage id="calificaciones" defaultMessage="Calificaciones" /></a></Link></Menu.Item>
-                            <Menu.Item key={`premios`}><Link href={`/${lang}/${sistemaActual}/editor/evaluacion/premios`}><a><FormattedMessage id="premios" defaultMessage="Premios" /></a></Link></Menu.Item>
-                            <Menu.Item key={`lista-de-control`}><Link href={`/${lang}/${sistemaActual}/editor/evaluacion/lista-de-control`}><a><FormattedMessage id="lista-de-control" defaultMessage="Lista de control" /></a></Link></Menu.Item>
-                            <Menu.Item key={`encuestas`}><Link href={`/${lang}/${sistemaActual}/editor/evaluacion/encuestas`}><a><FormattedMessage id="encuestas" defaultMessage="Encuestas" /></a></Link></Menu.Item>
-                            <Menu.Item key={`autoevaluacion`}><Link href={`/${lang}/${sistemaActual}/editor/evaluacion/autoevaluacion`}><a><FormattedMessage id="autoevaluacion" defaultMessage="Autoevaluacion" /></a></Link></Menu.Item>
-                            <Menu.Item key={`rubricas`}><Link href={`/${lang}/${sistemaActual}/editor/evaluacion/rubricas`}><a><FormattedMessage id="rubricas" defaultMessage="Rubricas" /></a></Link></Menu.Item>
+                            <Menu.Item key={`cuestionarios`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/evaluacion/cuestionarios`}><a><FormattedMessage id="cuestionarios" defaultMessage="Cuestionarios" /></a></Link></Menu.Item>
+                            <Menu.Item key={`buzon`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/evaluacion/buzon`}><a><FormattedMessage id="buzon" defaultMessage="Buzon" /></a></Link></Menu.Item>
+                            <Menu.Item key={`calificaciones`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/evaluacion/calificaciones`}><a><FormattedMessage id="calificaciones" defaultMessage="Calificaciones" /></a></Link></Menu.Item>
+                            <Menu.Item key={`premios`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/evaluacion/premios`}><a><FormattedMessage id="premios" defaultMessage="Premios" /></a></Link></Menu.Item>
+                            <Menu.Item key={`lista-de-control`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/evaluacion/lista-de-control`}><a><FormattedMessage id="lista-de-control" defaultMessage="Lista de control" /></a></Link></Menu.Item>
+                            <Menu.Item key={`encuestas`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/evaluacion/encuestas`}><a><FormattedMessage id="encuestas" defaultMessage="Encuestas" /></a></Link></Menu.Item>
+                            <Menu.Item key={`autoevaluacion`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/evaluacion/autoevaluacion`}><a><FormattedMessage id="autoevaluacion" defaultMessage="Autoevaluacion" /></a></Link></Menu.Item>
+                            <Menu.Item key={`rubricas`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/evaluacion/rubricas`}><a><FormattedMessage id="rubricas" defaultMessage="Rubricas" /></a></Link></Menu.Item>
                         </SubMenu>
-                        <SubMenu key="comunicacion" title={<FormattedMessage id="comunicacion" defaultMessage="Comunicación" />} className="menuTercerNivel">
-                            <Menu.Item key={`debates`}><Link href={`/${lang}/${sistemaActual}/editor/comunicacion/debates`}><a><FormattedMessage id="debates" defaultMessage="Debates" /></a></Link></Menu.Item>
-                            <Menu.Item key={`agentes-inteligentes`}><Link href={`/${lang}/${sistemaActual}/editor/comunicacion/agentes-inteligentes`}><a><FormattedMessage id="agentes-inteligentes" defaultMessage="Agentes inteligentes" /></a></Link></Menu.Item>
-                            <Menu.Item key={`aulas-virtuales`}><Link href={`/${lang}/${sistemaActual}/editor/comunicacion/aulas-virtuales`}><a><FormattedMessage id="aulas-virtuales" defaultMessage="Aulas virtuales" /></a></Link></Menu.Item>
-                            <Menu.Item key={`retroalimentacion`}><Link href={`/${lang}/${sistemaActual}/editor/comunicacion/retroalimentacion`}><a><FormattedMessage id="retroalimentacion" defaultMessage="Retroalimentación" /></a></Link></Menu.Item>
-                            <Menu.Item key={`video-tareas`}><Link href={`/${lang}/${sistemaActual}/editor/comunicacion/video-tareas`}><a><FormattedMessage id="video-tareas" defaultMessage="Video tareas" /></a></Link></Menu.Item>
+                        <SubMenu key="comunicacion" title={<FormattedMessage id="comunicacion" defaultMessage="Comunicación" />} className="menuTercerNivel" onTitleClick={this.onTitleSubMenuSegundoClick}>
+                            <Menu.Item key={`debates`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/comunicacion/debates`}><a><FormattedMessage id="debates" defaultMessage="Debates" /></a></Link></Menu.Item>
+                            <Menu.Item key={`agentes-inteligentes`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/comunicacion/agentes-inteligentes`}><a><FormattedMessage id="agentes-inteligentes" defaultMessage="Agentes inteligentes" /></a></Link></Menu.Item>
+                            <Menu.Item key={`aulas-virtuales`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/comunicacion/aulas-virtuales`}><a><FormattedMessage id="aulas-virtuales" defaultMessage="Aulas virtuales" /></a></Link></Menu.Item>
+                            <Menu.Item key={`retroalimentacion`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/comunicacion/retroalimentacion`}><a><FormattedMessage id="retroalimentacion" defaultMessage="Retroalimentación" /></a></Link></Menu.Item>
+                            <Menu.Item key={`video-tareas`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/comunicacion/video-tareas`}><a><FormattedMessage id="video-tareas" defaultMessage="Video tareas" /></a></Link></Menu.Item>
+                            <Menu.Item key={`herramientas-de-comunicacion`}><Link href={`/${lang}/${sistemaActual}/fase-edicion/herramientas-de-comunicacion`}><a>Herramientas de comunicación</a></Link></Menu.Item>
                         </SubMenu>
                     </SubMenu>
-                    {/* <SubMenu key="disenador" title={<FormattedMessage id="disenador" defaultMessage="Diseñador Gráfico" />} onTitleClick={() => Router.push(`/${lang}/${sistemaActual}/disenador-grafico/home`)}>                        
+                    <SubMenu key="fase-despliegue" title={<FormattedMessage id="fase-despliegue" defaultMessage="Fase de despliegue" />} onTitleClick={this.onTitleSubMenuClick}>                                                
+                        <Menu.Item key={`curso-tutoreado`}><Link href={`/${lang}/${sistemaActual}/fase-despliegue/curso-tutoreado`}><a><FormattedMessage id="curso-tutoreado" defaultMessage="Curso " /></a></Link></Menu.Item>
                     </SubMenu>
-                    <SubMenu key="desarrollador" title={<FormattedMessage id="desarrollador" defaultMessage="Desarrollador SW" />} onTitleClick={() => Router.push(`/${lang}/${sistemaActual}/desarrollador/home`)}>
-                        <Menu.Item key={`api`}><Link href={`/${lang}/${sistemaActual}/desarrollador/api`}><a>Api</a></Link></Menu.Item>
-                    </SubMenu> */}
+                    <SubMenu key="fase-ejecucion" title={<FormattedMessage id="fase-ejecucion" defaultMessage="Fase de ejecución" />} onTitleClick={this.onTitleSubMenuClick}>                                                
+                        <Menu.Item key={`informe-cierre`}><Link href={`/${lang}/${sistemaActual}/fase-ejecucion/informe-cierre`}><a><FormattedMessage id="informe-cierre" defaultMessage="Informe de cierre" /></a></Link></Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="fase-analisis" title={<FormattedMessage id="fase-analisis" defaultMessage="Fase de análisis" />} onTitleClick={this.onTitleSubMenuClick}>                        
+                        <Menu.Item key={`progreso`}><Link href={`/${lang}/${sistemaActual}/fase-analisis/progreso`}><a><FormattedMessage id="progreso-del-curso" defaultMessage="Progreso del curso" /></a></Link></Menu.Item>
+                    </SubMenu>
                 </Menu>
             </IntlProvider>
         } else if (sistemaActual == "tctp-catalogo-bs") {
---
+++ componentes/tctpMenuSuperior.js
@@ -0,0 +1,26 @@
+import {Icon, Menu } from 'antd'
+
+export default class extends React.Component {
+    
+    action=(e)=>{        
+        this.props.callback(e.key); // llama función del componente padre
+    }
+
+    render() {        
+        return (<Menu onClick={this.action} selectedKeys={[this.props.sistemaActual]} mode="horizontal" style={{ background: 'none', lineHeight: '60px' }}>
+            <Menu.Item key="tctp-lms-bs">
+                <Icon type="cloud-server" />
+                LMS Brightspace
+            </Menu.Item>
+            <Menu.Item key="tctp-catalogo-bs">
+                <Icon type="file-search" />
+                Catálogo Brightspace
+            </Menu.Item>
+            <Menu.Item key="tctp-comunidad-hh">
+                <Icon type="team" />
+                Comunidad Humhub
+            </Menu.Item>
+        </Menu>
+        )
+    }
+}
