23374095f54d1be3fe525293a5bf466af7725c3d
solivares1401 <seba_olivares@live.cl>
2019-12-05 09:59:57 -0300

    servicio cleanStrings

--- componentes/documentacion/tctpRolesPermisos.js
+++ componentes/documentacion/tctpRolesPermisos.js
@@ -1,5 +1,6 @@
 import { Select, Icon, Table } from 'antd';
 import { rolesPermisosServices } from '../../services/rolesPermisosServices';
+import { cleanStrings } from '../../services/cleanStrings';
 import Loading from '../loading';
 const { Option } = Select;
 
@@ -59,7 +60,7 @@ export default class extends React.Component {
             },
           ];
           arrOusFiltradas.forEach((ou)=>{                
-                columns.push({title:ou.name, dataIndex:this.getCleanedString(ou.name), align: 'center', width:"12%"});                
+                columns.push({title:ou.name, dataIndex:cleanStrings.getCleanedString(ou.name), align: 'center', width:"12%"});                
           })
 
            //data para construir tabla
@@ -104,27 +105,6 @@ export default class extends React.Component {
             this.setState({ allRolesByTool: resp, loading: false, error: false });
         }
     }
-
-    getCleanedString(cadena){
-        // Definimos los caracteres que queremos eliminar
-        var specialChars = "!@#$^&%*()+=-[]\/{}|:<>?,.";     
-        // Los eliminamos todos
-        for (var i = 0; i < specialChars.length; i++) {
-            cadena= cadena.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
-        }        
-        // Lo queremos devolver limpio en minusculas
-        cadena = cadena.toLowerCase();     
-        // Quitamos espacios y los sustituimos por _ porque nos gusta mas asi
-        cadena = cadena.replace(/ /g,"");     
-        // Quitamos acentos y "ñ". Fijate en que va sin comillas el primer parametro
-        cadena = cadena.replace(/á/gi,"a");
-        cadena = cadena.replace(/é/gi,"e");
-        cadena = cadena.replace(/í/gi,"i");
-        cadena = cadena.replace(/ó/gi,"o");
-        cadena = cadena.replace(/ú/gi,"u");
-        cadena = cadena.replace(/ñ/gi,"n");
-        return cadena;
-     }
 
      setIconsIsAllowed(allowed){
          if(allowed){
